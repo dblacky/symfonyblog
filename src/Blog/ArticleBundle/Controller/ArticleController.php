cf7f39e4193212c9ef53f2ebec17d758ac3043b3
atos_portos <atos.kg@gmail.com>
2015-09-19 15:58:22 +0600

    Добавлено редактирование статей

--- src/Blog/ArticleBundle/Controller/ArticleController.php
+++ src/Blog/ArticleBundle/Controller/ArticleController.php
@@ -50,7 +50,7 @@ class ArticleController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($article);
             $em->flush();
-            return $this->redirect($this->generateUrl('article_homepage'));
+            return $this->redirect($this->generateUrl('article_post', array("id"=> $article->getId())));
         }
 
         return $this->render('BlogArticleBundle:Article:create.html.twig', array(
@@ -59,5 +59,28 @@ class ArticleController extends Controller
         ));
 
 
+    }
+
+    public function editAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository("BlogArticleBundle:Article")->find($id);
+        if (!$article){
+            throw $this->createNotFoundException("Article not found");
+        }
+        $form = $this->createForm(new ArticleType(), $article);
+        $form->add("submit", "submit", array(
+            "label" => "Редактировать"));
+
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $em->flush();
+            return $this->redirect($this->generateUrl("article_post", array("id" =>$article->getId())));
+        }
+
+        return $this->render("BlogArticleBundle:Article:edit.html.twig", array(
+            'article' => $article,
+            'editform' => $form->createView(),
+        ));
     }
 }
